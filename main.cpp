c04ace39b3db1d759a5470107efbad0598328820
Hammania689 <hameedabdulrashid@gmail.com>
2017-08-18 14:03:08 -0500

    Finished basic program. Would like to make file print function, fix percision output, and create loop to call that function until the end of that file.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,17 @@
 #include <fstream>
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
-//
+// Use to calculate NetIncome after taxes and bonuses
+double NetIncome(double &, double &, double &);
+
+// Use to calculate Average Speed
+double AvgSpeed(double &d, double &h);
+
+// Use to calculate Gross Sales Amount
+double SalesAmt(double &, double &);
 
 int main()
 {
@@ -21,22 +29,51 @@ int main()
             avgSpeed = 0.0, salesAmt = 0.0;
 
     // Accessing the input data
+    // Should be stored in a loop to have everything complete
     getline(inData,name);
-    inData >> gross >> bonus >> taxes;
+    inData >> gross >> bonus >> taxes
+           >> distance >> hours >> cupsSold >> costPerCup;
 
-    // Could turn this into a oneline function for better readability
-    paycheck = ((gross * bonus/100) + gross) * ((100 - taxes)/100);
+    // Store the function return values in categorical values
+    avgSpeed = AvgSpeed(distance,hours);
+    salesAmt = SalesAmt(cupsSold,costPerCup);
+    paycheck = NetIncome(gross, bonus, taxes);
 
-    // Ensure the readstream opened up correctly
-    if(!inData)
-        outData << "Nothing to show.. Try Agian \n";
 
     // If everything looks good then carry on and output the data
-    outData << "Name: " << name << endl;
-    outData << "PayCheck :" << paycheck << endl;
+    if(inData)
+    {
+            outData << "Name: " << name << endl;
+            outData << "Monthly Gross Salary: $" << gross
+                    << ", Monthly Bonus: " << bonus << "%, " << "Taxes: " << taxes <<"%" << endl;
+            outData << "NetIncome : $" << paycheck << endl << endl;
+
+            outData << "Distance Traveled: " << distance << " miles, Traveling Time: " << hours
+                    << " hours" << endl << "Average Speed: " << avgSpeed << " miles per hour" << endl << endl;
+
+            outData << "Number of Coffee Cups Sold: " << cupsSold << ", Cost: $" << costPerCup << " per cup" << endl
+                    << "Sales Amount: $" << salesAmt << endl << endl;
+    }
+    else // Ensure the read stream opened up correctly
+        outData << "Nothing to show.. Try Again \n";
 
     // Safely close the files
     inData.close();
     outData.close();
     return 0;
 }
+
+double SalesAmt(double &sold, double &cupCost)
+{ return sold*cupCost;
+}
+
+double AvgSpeed(double &d, double &h)
+{
+    return d / h;
+}
+
+double NetIncome(double &g, double &b, double &t)
+{
+    return (((g * b / 100) + g) * ((100 - t) / 100));
+}
+
