2d7ab6b19c1783c22791159e39cfe2698e19dc71
Hammania689 <hameedabdulrashid@gmail.com>
2017-08-28 23:53:26 -0500

    Finished first assignment last night pretty much. Feeling pretty damn good about myself about now :)
    Signed-off-by: Hammania689 <hameedabdulrashid@gmail.com>

--- Data Structures/Text and Binary Files/BinaryReadAndAssignment.cpp
+++ Data Structures/Text and Binary Files/BinaryReadAndAssignment.cpp
@@ -12,41 +12,139 @@
 
 using namespace std;
 
-int main()
-{
-    auto line = std::unique_ptr<char*>(new char*);
+// Use to read binary file, store data into a int* (Unique Ptr - handles malloc)
+// Takes a file( built for Binary files) and two integers all by reference
+// Returns int* with stored Record Data
+// Will be used to store Record data in subsequent binary and text files
+std::unique_ptr<int[]> ReadRecord(std::fstream &bin, int &recs, int &t);
+
+// Use to write data to data to new binary and text files
+// Parameters: original binary file (Data.in), a new binary file, a new text file, and the number of current records
+void WriteToFiles(std::unique_ptr<int[]> &data, std::fstream &newBin, fstream &txt, int &recs);
+
+void CheckSmartPointerData(std::unique_ptr<int[]> &ptr, int &recs);
+
+int main(int argc, char *argv[]) {
+    // Fstreams to files and binary files
+    fstream textFile("DataOutput.txt", ios::out);
+
+    // Variables that correlate to Record data present in Datafile
     auto temp = 0, salinaty = 0, height = 0, numOfRecs = 0;
 
-    fstream dataFile("data.bin", ios::binary | ios::in );
+    // Error Insufficient number of Parameters
+    if (argc != 2) {
+        cout << "Error; Usage: " << argv[0] << " <filename> \n";
+    } else {
+        fstream dataFile(argv[1], ios::binary | ios::in);
 
-    // Ensure that the file is open
-    if(dataFile.is_open())
-    {
+        // Point to the very beginning of the file
         dataFile.seekg(0);
-        dataFile.read((char *) &temp ,4);
-        dataFile.read((char *) &salinaty ,4);
-        dataFile.read((char *) &height ,4);
-        dataFile.read((char *) &numOfRecs, 4);
 
-        // Check to see if something broke in the process of reading
-        if(dataFile.fail())
-        // ERROR: COULD NOT READ THE FILE PROPERLY CHECK
-        // CHECK PREVIOUS LINES OF CODE
-            return 2;
+        // Ensure that the file is open
+        if (dataFile.is_open()) {
+            // Smart pointer dedicated to hold current Record Variable Values
+            auto ptr = std::unique_ptr<int[]>();
 
-        // Output data to the screen
-        cout << temp << salinaty << height << numOfRecs << " ";
+            // Read File metadata to determine which variables are relevant
+            dataFile.read((char *) &temp, 4);
+            dataFile.read((char *) &salinaty, 4);
+            dataFile.read((char *) &height, 4);
+            dataFile.read((char *) &numOfRecs, 4);
 
-        // Collect all records of relevant data
-        for(auto i = 0; i < numOfRecs; i++)
-        {
-            dataFile.read((char *) &salinaty ,4);
-            cout << salinaty << " ";
+            // Check to see if something broke in the process of reading
+            // ERROR: COULD NOT READ THE FILE PROPERLY CHECK
+            // CHECK PREVIOUS LINES OF CODE
+            if (dataFile.fail()) {
+                return 2;
+            }
+
+            // Output data to the screen
+            cout << temp << salinaty << height << numOfRecs << " ";
+
+            if (textFile) {
+                if (temp != 0) {
+                    ptr = move(ReadRecord(dataFile, numOfRecs, temp));
+                    textFile << "Temp: ";
+                    fstream newBin(to_string(temp) + ".bin", ios::binary | ios::out);
+                    cout << endl << endl << "Temperature : \n";
+                    if (newBin.fail()) {
+                        return 2;
+                    }
+                    WriteToFiles(ptr, newBin, textFile, numOfRecs);
+                    newBin.close();
+                }
+                if (salinaty != 0) {
+                    ptr = move(ReadRecord(dataFile, numOfRecs, salinaty));
+                    textFile << "Salinaty: ";
+                    cout << endl << endl << "Salinaty: \n";
+                    fstream newBin("salinaty.bin", ios::binary | ios::out);
+                    if (newBin.fail()) {
+                        return 2;
+                    }
+                    WriteToFiles(ptr, newBin, textFile, numOfRecs);
+                    newBin.close();
+                }
+                if (height != 0) {
+                    ptr = move(ReadRecord(dataFile, numOfRecs, height));
+                    textFile << "Height :";
+                    cout << endl << endl << "Height: \n";
+                    fstream newBin("height.bin", ios::binary | ios::out);
+                    if (newBin.fail()) {
+                        return 2;
+                    }
+                    WriteToFiles(ptr, newBin, textFile, numOfRecs);
+                    newBin.close();
+                }
+            } else {
+                return 3;
+            }
+
+            // Check to see that the unique ptr transfer happen
+        } else {
+            // ERROR: FILE COULD NOT BE OPENED
+            return 1;
         }
+        dataFile.close();
     }
-    else
-        // ERROR: FILE COULD NOT BE OPENED
-        return 1;
+
+    //CheckSmartPointerData(ptr, numOfRecs);
+    textFile.close();
+
     return 0;
 }
 
+void CheckSmartPointerData(std::unique_ptr<int[]> &ptr, int &recs) {
+// Check to see that the unique ptr transfer happen
+    cout << endl << endl << endl << endl << "Unique pointer data :" << endl;
+    for (auto i = 0; i < recs; i++)
+        cout << ptr[i] << " ";
+}
+
+// Use to Read and store data(OWNERSHIP MUST BE PASSED) into smart pointer for later use
+std::unique_ptr<int[]> ReadRecord(std::fstream &bin, int &recs, int &t) {
+    // Original Pointer owner of Record data
+    auto p = std::make_unique<int[]>(recs);
+
+    for (auto i = 0; i < recs; i++) {
+        // Read file in increments of 4 bytes
+        // Store Variable at proper index
+        bin.read((char *) &t, 4);
+        p[i] = t;
+    }
+    // Returns information stored into p
+    return p;
+}
+
+// Takes UniquePtr and passes array values to the binary and text file through loop
+void WriteToFiles(std::unique_ptr<int[]> &data, std::fstream &newBin, fstream &txt, int &recs) {
+    for (auto i = 0; i < recs; i++) {
+        newBin.write((char *) &data[i], sizeof(int));
+        txt << data[i] << " ";
+        cout << data[i] << " ";
+    }
+    txt << endl;
+
+    if (newBin.fail()) {
+        cout << "Error while trying to write data to the new Binary file" << endl;
+    }
+}
